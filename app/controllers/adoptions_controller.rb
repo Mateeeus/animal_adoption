d8f1e9989470ee4b06c1f2a29b6576e8b7037d27
Mateus Nogueira <mnogueira030@gmail.com>
2021-01-27 10:16:11 -0300

    adoptions_controller
    updating adoptions_controller

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -1,4 +1,7 @@
 class AdoptionsController < ApplicationController
+
+  before_action :current_adoption, only: [ :show, :edit, :update, :destroy ]
+
   def index
   end
 
@@ -15,8 +18,23 @@ class AdoptionsController < ApplicationController
   end
 
   def update
+    @adoption.update(adoption_params)
+
+    redirect_to animals_path
   end
 
   def destroy
   end
+
+  private
+
+  def adoption_params
+    params.require(:adoption).permit(:owner_name, :date_of_adoption)
+  end
+
+  def current_adoption
+    @adoption = Adoption.find(params[:id])
+    @animal = Animal.where(id: @adoption.animal_id).first
+  end
+
 end
