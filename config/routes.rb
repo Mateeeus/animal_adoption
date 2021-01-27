76a81a13839a62f2ea643c1cc33be5838707dad8
Mateus Nogueira <mnogueira030@gmail.com>
2021-01-27 09:35:16 -0300

    routes
    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'animals/index'
-  get 'animals/show'
-  get 'animals/new'
-  get 'animals/create'
-  get 'animals/edit'
-  get 'animals/update'
-  get 'animals/destroy'
+  #get 'animals/index'
+  #get 'animals/show'
+  #get 'animals/new'
+  #get 'animals/create'
+  #get 'animals/edit'
+  #get 'animals/update'
+  #get 'animals/destroy'
+  resources :animals
+
+  root to: "animals#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
